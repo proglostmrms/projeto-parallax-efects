4f20345c79ca400d754b0e013ecb33d99e13bcbf
Marcelo Rullo Marques <marcelo.titecnologia@gmail.com>
2022-05-27 11:42:38 -0300

    atualizado

---
+++ script.js
@@ -0,0 +1,4 @@
+window.sr = ScrollReveal({ reset: true});
+sr.reveal('.titulo'), {duration: 1000};
+sr.reveal('.titulo1', { rotate: { x: 0, y: 80, z: 0}, duration: 2000});
+sr.reveal('.titulo2', { rotate: { x: 100, y: 100, z: 0}, duration: 2000,});
